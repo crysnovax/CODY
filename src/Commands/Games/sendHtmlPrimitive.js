06af038ed85970183ea4efe5cf4b23125b21db9d
CRYSTAL-LEVI UZOUKWU <crysnovax@gmail.com>
2026-09-20 04:42:31 +0100

    Update and rename sendHtmlPrimitive (1).js to sendHtmlPrimitive.js

--- src/Commands/Games/sendHtmlPrimitive.js
+++ src/Commands/Games/sendHtmlPrimitive.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const { generateWAMessageFromContent } = require('@crysnovax/baileys');
+const { generateWAMessageFromContent } = require('plogme');
 
 /**
  * Sends an HTML payload through the FOAHtmlPrimitiveDemoDONOTUSE rich-response
